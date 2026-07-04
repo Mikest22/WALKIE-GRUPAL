5c771daa47d5f02afa0f62d93cb100ded3ee4532
Mikest22 <saldarinmiguelct@icloud.com>
2026-07-04 06:37:14 -0600

    Crear firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDOZBVsRy8vzDl8YSrYkoOdT2kQvCP1DLw",
+  authDomain: "walkie-pro-1945a.firebaseapp.com",
+  databaseURL: "https://walkie-pro-1945a-default-rtdb.firebaseio.com/",
+  projectId: "walkie-pro-1945a",
+  storageBucket: "walkie-pro-1945a.firebasestorage.app",
+  messagingSenderId: "394474795757",
+  appId: "1:394474795757:web:275208911e6cb14757d2e4"
+};
